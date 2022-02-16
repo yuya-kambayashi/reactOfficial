1961ec62e3d190acd7e31d60512c9d460413776a
yuya-kambayashi <yuya-kambayashi@kke.co.jp>
2022-02-16 16:18:36 +0900

    useStateを用いたカウンタを実装します

---
+++ react-official/src/Counter.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './styles.css';
+
+const Counter = () => {
+    // countの初期値として、1~10までのランダムな数値を生成
+    const initialState = Math.floor(Math.random() * 10) + 1
+    // count という名前の state 変数を宣言、初期値 initialState をセット
+    const [count, setCount] = useState<number>(initialState)
+    // open という名前の state 変数を宣言、初期値 true をセット
+    const [open, setOpen] = useState<boolean>(true)
+    // toggleの関数を宣言
+    const toggle = () => setOpen(!open)
+  
+    return (
+      <>
+        <button onClick={toggle}>{open ? 'close' : 'open'}</button>
+        <div className={open ? 'isOpen' : 'isClose'}>
+          <p>現在の数字は{count}です</p>
+          {/* setCount()は、countを更新するための関数。countを引数で受け取ることも出来る */}
+          <button onClick={() => setCount(prevState => prevState + 1)}>
+            + 1
+          </button>
+          <button onClick={() => setCount(count - 1)}>- 1</button>
+          <button onClick={() => setCount(0)}>０</button>
+          <button onClick={() => setCount(initialState)}>最初の数値に戻す</button>
+        </div>
+      </>
+    )
+  }
+  
+
+export default Counter;
